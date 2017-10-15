0001c78682b45d7626d2e0ea21c332f78069b597
quinnnned <quinnnned@gmail.com>
2017-10-15 16:05:08 -0500

    Refactor AddSubordinateForm to improve testability

--- src/view/AddSubordinateForm.js
+++ src/view/AddSubordinateForm.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import * as UI from 'react-materialize';
 
+export const handleSubmit = (person) => (event) => { 
+    event.preventDefault(); 
+    const field = event.target[0];
+    person.addSubordinate(field.value);
+    field.value = ''
+}
+
 export default function AddSubordinateForm(props) {
     const {person} = props;
     return (
         <UI.Row>
             <form 
-                onSubmit={ e => { 
-                    e.preventDefault(); 
-                    const field = e.target[0];
-                    person.addSubordinate(field.value); 
-                    field.value = ''
-                }}
+                onSubmit={handleSubmit(person)}
                 >
                 <UI.Input 
                     label="Subordinate Name"
