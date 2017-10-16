ec4814984daee2c744334cb2a8a6f0ed7f9d07d3
quinnnned <quinnnned@gmail.com>
2017-10-15 21:46:15 -0500

    Enhance any.state Generator

--- src/test/properties/generators.js
+++ src/test/properties/generators.js
@@ -1,5 +1,5 @@
-import {gen} from 'testcheck'
-const any = {...gen}
+import {gen, sampleOne} from 'testcheck'
+const any = Object.assign(gen.any, gen)
 export default any
 
 any.employeeId =
@@ -9,10 +9,33 @@ any.name =
     any.asciiString.notEmpty()
 
 any.state = 
-    any.object({
-        selectedEmployeeId: 
-            any.oneOf([
-                any.employeeId, 
-                null
-            ])
-    })
+    any.uniqueArray(any.employeeId, { maxSize: 10 })
+        .then( employeeIds => {
+            return {
+                selectedEmployeeId: (
+                    employeeIds.length === 0 ? null : (
+                        // Pick a random employee id to be selected
+                        employeeIds[
+                            sampleOne(any.intWithin(0, employeeIds.length-1))
+                        ]    
+                    )
+                ),
+                employeeIds,
+                employeesById: (
+                    employeeIds.reduce( (employeesById, employeeId, index) => {
+                        return {
+                            ...employeesById,
+                            [employeeId]: {
+                                employeeName: sampleOne(any.name),
+                                supervisor: (index == 0) ? null : (
+                                    // Grab a random earlier employeeId
+                                    employeeIds[
+                                        sampleOne(any.intWithin(0, index - 1))
+                                    ]
+                                )
+                            }
+                        }
+                    }, {})
+                )
+            }
+        })
